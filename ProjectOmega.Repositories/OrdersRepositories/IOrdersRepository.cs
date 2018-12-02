46d8e0cefc36c25082fedafde616cd324d6556bf
Maciek <maciek.d@me.com>
2018-12-02 18:40:38 +0100

    CRUD for Orders in DB added

--- ProjectOmega.Repositories/OrdersRepositories/IOrdersRepository.cs
+++ ProjectOmega.Repositories/OrdersRepositories/IOrdersRepository.cs
@@ -8,5 +8,6 @@ namespace ProjectOmega.Repositories.OrdersRepositories
         void Create(AddOrderModel order);
         void Update(EditOrderModel order);
         IEnumerable<OrderModel> GetAll();
+        OrderModel GetById(long id);
     }
 }
---
+++ ProjectOmega.Repositories/OrdersRepositories/SqlOrdersRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectOmega.Data.Models.Order;
+using ProjectOmega.DAL.MsSql.Services;
+
+namespace ProjectOmega.Repositories.OrdersRepositories
+{
+    public class SqlOrdersRepository : IOrdersRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public SqlOrdersRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(AddOrderModel order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EditOrderModel order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OrderModel> GetAll()
+        {
+            return _context.Orders.Select(x => new OrderModel()
+            {
+                Number = "MockedNumber",
+                ClientId = x.ClientId,
+                UserAdded = x.UserAdded,
+                UserResponsible = x.UserResponsible,
+                UserInvoice = x.UserInvoice,
+                Status = x.Status
+            });
+        }
+
+        public OrderModel GetById(long id)
+        {
+           var order =  _context.Orders.SingleOrDefault(x => x.Id == id);
+            return new OrderModel()
+            {
+                Number = order.Number,
+                ClientId = order.ClientId,
+                UserAdded = order.UserAdded,
+                UserResponsible = order.UserResponsible,
+                UserInvoice = order.UserInvoice,
+                Status = order.Status
+            };
+        }
+    }
+}
+
